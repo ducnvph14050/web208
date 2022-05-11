7719c9ab67954e12ce229fda0e60e5ca19def086
Duc Mai Thanh <ducmtph14090@fpt.edu.vn>
2022-05-11 20:40:17 +0700

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'hello', component: HelloComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
